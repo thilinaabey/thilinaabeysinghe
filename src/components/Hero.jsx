0c7a93fc0f8560eed4427a378eb1c5a472ffdfe0
Thilina Abeysinghe <107211493+thilinaabey@users.noreply.github.com>
2025-03-03 15:40:48 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,6 +46,9 @@ const Hero = () => {
           <img className=' w-[300px] sm:w-[500px] mx-auto h-auto' src={profilepic} alt="profile pic" />
         </div>
 
+          <div>
+            <iframe src="https://www.fiverr.com/gig_widgets?id=U2FsdGVkX19qHNvrvJ1KbBps9IWZWkuMtGm7ntzOdEDMn/4fGw3mO8UbOqieSxnf4ibXXh9MZL/rO/jZuXSj6Ls1fxEfRVmjka3MSQrxumwaa/MFm8jx9bfoyKiRD2vIkdZJd53S0M3i9Kzl+2Nn+Htc0ba5jhO6qWP2O9PcXkEJhFKssVxEmg2hOOSk3qPvIvwq6X0qY4S4U7gqJ+XQl9kaDJXrSJa9CaA+CorOXa+MEgfwyBB6zrI8lGyLBwS5e7HATWj17Hn8M57ncwz7Vy1SQ+Q9iHTy/oWehJlVdTry+UuKb16ooSLdERT3Qm5QntKpvcCP/9H2nuVFhi21pYU3nhq+mkUZKufPsqNHqC3wEDwfcCrT2jKkn87XlQLsJDzBE0lKuiKNAbspp79RLH6A3oZvN4v9mlCOzxcM1LZNFne8Obn4I7rLc2GRbXQNIGA34DVPonsiNtsVzkxgm3LsE9N4BNJWkRhLYnK/7hiCK/J8D5CrbF5EYIkrOD+lfODxwXCchNzFaoTthKRn3Pg+OLWxZ3GstZ5jvGvKpFOABegISa2f+WJw8PGQWDHZ&affiliate_id=578200&strip_google_tagmanager=true" loading="lazy" data-with-title="true" class="fiverr_nga_frame" frameborder="0" height="350" width="100%" referrerpolicy="no-referrer-when-downgrade" data-mode="specific_gig" onload=" var frame = this; var script = document.createElement('script'); script.addEventListener('load', function() { window.FW_SDK.register(frame); }); script.setAttribute('src', 'https://www.fiverr.com/gig_widgets/sdk'); document.body.appendChild(script); " ></iframe>
+          </div>
         
         </div>
     </div>
